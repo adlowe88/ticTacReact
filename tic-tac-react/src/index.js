951a6694d38b18b2d6c735cc3a15034b5844c8ae
Damien Lowe <ashleydamienlowe@gmail.com>
2017-12-08 15:44:06 +1100

    Board definition

--- tic-tac-react/src/index.js
+++ tic-tac-react/src/index.js
@@ -1,8 +1,71 @@
-import React from 'react';
+import React, { PureComponent} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+class Square extends PureComponent {
+  render() {
+    return (
+      <button className = "square">
+        {this.props.value}
+      </button>
+    );
+  }
+}
+
+class Board extends PureComponent {
+  renderSquare( i ) {
+    return <Square value = { i } />;
+  }
+
+  render() {
+    const status = "Next player: X";
+
+    return (
+      <div>
+        <div className = "status"> { status } </div>
+
+        <div className = "board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+
+        <div className = "board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+
+        <div className = "board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+
+      </div>
+    );
+  }
+}
+
+class Game extends PureComponent {
+  render() {
+    return (
+      <div className = "game">
+        <div className = "game-board">
+        <Board />
+        </div>
+
+        <div className = "game-info">
+          <div>{}</div>
+          <ol>{}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
